d16fe195100dfe72467632ab85528c7deada0de3
kamalkumarde <madhavankamalkumar@gmail.com>
2026-02-03 02:14:45 +0100

    chore: remove tracked files that should be ignored

--- target/compiled/enpal_assessment_project/models/intermediate/int_field_configs.sql
+++
@@ -1,7 +0,0 @@
-select
-    field_key,
-    name as field_display_name,
-    elem ->> 'id' as option_id,
-    elem ->> 'label' as option_name
-from "postgres"."public"."fields"
-cross join lateral jsonb_array_elements(field_value_options) as elem
